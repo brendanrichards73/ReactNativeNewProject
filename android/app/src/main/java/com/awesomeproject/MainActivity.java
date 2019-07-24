f913f7183f5cf64362dc9edb22a0bb96abdd7417
brendan richards <brendan.richards@btinternet.com>
2019-07-24 11:03:22 +0100

    created MyFirebaseMessagingService.java file

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -24,5 +24,25 @@ public class MainActivity extends ReactActivity {
             return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
+
+    FirebaseInstanceId.getInstance().getInstanceId()
+        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "getInstanceId failed", task.getException());
+                    return;
+                }
+
+                // Get new Instance ID token
+                String token = task.getResult().getToken();
+
+                // Log and toast
+                String msg = getString(R.string.msg_token_fmt, token);
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
   }
+  
 }
